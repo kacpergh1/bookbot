10cf2e3b0684f353b5e214879541427f00eba249
kacpergh1 <kacperzapa@gmail.com>
2024-10-16 22:34:09 +0200

    python part 2

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ def main():
     path_to_file = "frankenstein.txt"
     with open(path_to_file) as f:
         file_contents = f.read()
-        print(file_contents)
+        
+        number_of_words = len(file_contents.split())
+        print(number_of_words)
+        
+
 
 if __name__ == "__main__":
     main()
+
+
