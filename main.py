cbb671b567fa1be6873c8b0fb385739faaac7559
kacpergh1 <kacperzapa@gmail.com>
2024-10-16 21:56:53 +0200

    changed path to file

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 def main():
-    path_to_file = "books/frankenstein.txt"
+    path_to_file = "/root/workspace/github.com/kacpergh1/bookbot/books/frankenstein.txt"
     with open(path_to_file) as f:
         file_contents = f.read()
         print(file_contents)
 
 if __name__ == "__main__":
     main()
-    
