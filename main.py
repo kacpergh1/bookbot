3a0d273d0e2a28ff069014ab8ff1431d6f6fec70
kacpergh1 <kacperzapa@gmail.com>
2024-10-17 18:59:41 +0200

    copy pasted full

--- main.py
+++ main.py
@@ -1,22 +1,53 @@
 def main():
-    path_to_file = "frankenstein.txt"
-    with open(path_to_file) as f:
-        
-        file_contents = f.read()
-        number_of_words = len(file_contents.split())
-        
-    character_number = {}
-    for character in file_contents:
-        character = character.lower()
-        if character in character_number:
-            character_number[character] += 1
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
         else:
-            character_number[character] = 1
-    
-    character_number.sort(reverse=True)
-    print(character_number)
+            chars[lowered] = 1
+    return chars
 
 
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
-if __name__ == "__main__":
-    main()
+
+main()
