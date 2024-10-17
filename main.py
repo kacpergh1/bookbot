5d565414926354ca501e2343181419d7c0abbb37
kacpergh1 <kacperzapa@gmail.com>
2024-10-17 18:27:51 +0200

    character count 1st try

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 def main():
     path_to_file = "frankenstein.txt"
     with open(path_to_file) as f:
-        file_contents = f.read()
         
+        file_contents = f.read()
         number_of_words = len(file_contents.split())
         print(number_of_words)
-        
+    character_number = {}
+    for character in file_contents:
+        character = character.lower()
+        if character in character_number:
+            character_number[character] += 1
+        else:
+            character_number[character] = 1
+
 
 
 if __name__ == "__main__":
     main()
-
-
